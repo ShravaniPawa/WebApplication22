f16ab590809b930484da0c1e433ea207b8f47da4
Shravani pawar <pawarshravani548@gmail.com>
2023-04-03 16:16:44 +0530

    Revert "Task"
    This reverts commit f3cde5f3572363e56a12a7881dd90923997c7126.

--- WebApplication22/App_Start/RouteConfig.cs
+++ WebApplication22/App_Start/RouteConfig.cs
@@ -13,7 +13,6 @@ namespace WebApplication22
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
